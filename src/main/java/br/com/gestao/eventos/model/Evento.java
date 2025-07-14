b8683fc4fd378bc9bb5e3be96ff087d7461c2a52
rodrigocesarar <rodrigocesarar@gmail.com>
2025-07-14 20:01:09 -0300

    Criando entidade

--- src/main/java/br/com/gestao/eventos/model/Evento.java
+++ src/main/java/br/com/gestao/eventos/model/Evento.java
@@ -1,7 +1,47 @@
 package br.com.gestao.eventos.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Data
 public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String titulo;
+
+    @Size(max = 1000)
+    private String descricao;
+
+    @FutureOrPresent
+    private LocalDateTime dataHora;
+
+    @Size(max = 200)
+    private String local;
+
+    private Boolean deletado = false;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
